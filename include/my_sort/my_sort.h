dfe8af3b5ae9cbed9f8a92ff676c2b52e3730148
Vladislav <WWolkoW04@yandex.ru>
2023-11-30 19:21:39 +0300

    Add project files.

---
+++ include/my_sort/my_sort.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <vector>
+using std::vector;
+
+namespace my_sort {
+	
+}
---
+++ tests/my_sort_tests.cpp
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+#include "my_sort/my_sort.h"
+#include <iostream>
+
+using namespace my_sort;
